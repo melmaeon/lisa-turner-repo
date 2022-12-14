0817944a16617ecbb9813194181c4915c7dea3d6
melmaeon <itsmeyourduke@gmail.com>
2022-12-13 21:03:53 -0500

    work up to now

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,25 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  {/* -- service carousel  */}
+
   return (
     <div className="App">
-      
+      <div className="wrapper">
+        <div className="service-module">
+          <section className="service-home-cards">
+            <p className="service-home-cards-top-text">Read Now</p>
+            <h2 className="service-home-cards-top-header">Services</h2>
+            <div className="service-home-cards-carousel">
+              <div className="service-home-cards-arrows">
+                <span className="service-home-cards-arrow-left" data-glide-dir="<">::before</span>
+                <span className="service-home-cards-arrow-right" data-glide-dir=">">::before</span>
+              </div>
+              <div className="service-home-cards-carousel-track">{/* refer to template site for help here */}</div>
+            </div>
+          </section>
+        </div>
+      </div>
     </div>
   )
 }
